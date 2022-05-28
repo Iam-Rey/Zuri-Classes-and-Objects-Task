3f2885fcdbf6a461ff56cfd9938f0e38f83a1a97
Rey <105178384+Iam-Rey@users.noreply.github.com>
2022-05-28 15:10:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, track, score):
+        self.name = name
+        self.age = age
+        self.track = track
+        self.score = score
+        
+    def displayscore(self):
+        print(self.score)
+
+
+
+Bob = Student("Bob", 26, "FE,BE", 20.90)
+# Expected methods
+Bob.change_name = "Peter"
+Bob.change_age = 34
+Bob.add_track = "UI/UX"
+print(Bob.change_name)
+print(Bob.change_age)
+print(Bob.add_track)
+print(getattr(Bob, 'score'))
